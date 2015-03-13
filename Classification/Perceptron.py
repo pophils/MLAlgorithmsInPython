63245321e2542ff0ba25d5e6d3794f44879ca64e
Popoola Oluwaseun <seunpopoola@outlook.com>
2015-03-13 11:20:37 +0100

    Old biasing is slower.

--- Classification/Perceptron.py
+++ Classification/Perceptron.py
@@ -28,6 +28,7 @@ class Perceptron():
         self.__eta = eta
         self.__weight = self.__averageWeight = self.__aggregateWeight = self.__trainingSet = self.__labels = self.__error = None
         self.__numberOfTrainingSamples = self.__numOfFeatures = self.__numberOfLabels = 0
+        self.__bias = NP.zeros((1, 1))
         self.__averageWeighted = averageWeighted
 
     def fit_transform(self, trainingSet, trainingLabels):
@@ -35,9 +36,7 @@ class Perceptron():
         Trains the learning algorithm.
         :param trainingSet: The training sets (numpy array).
         :param trainingLabels: The training labels (numpy array).
-        :return: tuple (weight vector, error list)
         """
-
         if not isinstance(trainingSet, NP.ndarray):
             self.__trainingSet = NP.array(trainingSet)
         else:
@@ -59,29 +58,24 @@ class Perceptron():
         if self.__numberOfLabels != self.__numberOfTrainingSamples:
             raise ValueError("The number of training labels and set are not the same.")
 
-        # init all weights and bias.from
-        self.__weight = self.__aggregateWeight = NP.zeros(self.__numOfFeatures + 1)  # the increment is for the bias term
-        self.__trainingSet = NP.concatenate((self.__trainingSet, NP.ones((self.__numberOfTrainingSamples, 1))), axis=1)
+        # init all weights
+        self.__weight = self.__aggregateWeight = NP.zeros(self.__numOfFeatures)
 
         for run in range(self.__epoch):
             for cursor in range(self.__numberOfTrainingSamples):
-                prediction = Perceptron.__unitStep(NP.dot(self.__weight, self.__trainingSet[cursor, :]))
+                prediction = Perceptron.__unitStep(NP.dot(self.__weight, self.__trainingSet[cursor, :]) + self.__bias)
 
                 if prediction != self.__labels[cursor]:
                     error = self.__labels[cursor] - prediction
 
-                    if self.__error is None:
-                        self.__error = []
-                    self.__error.append(error)
+                    if self.__averageWeighted:
+                        self.__aggregateWeight = self.__aggregateWeight + self.__weight
 
-                    # self.__aggregateWeight += self.__weight this will cause overflow runtime error
-                    self.__aggregateWeight = self.__aggregateWeight + self.__weight
                     self.__weight += self.__eta * self.__trainingSet[cursor, :] * error
+                    self.__bias += self.__eta * self.__bias * error
 
         if self.__averageWeighted:
             self.__weight = self.__aggregateWeight / (self.__epoch * self.__numberOfTrainingSamples)
-
-        return self.__weight, self.__error
 
     def predict(self, testSet):
         """
@@ -103,13 +97,10 @@ class Perceptron():
         if self.__numOfFeatures != numOfTestFeatures:
             raise ValueError("The number of training and test features are not the same.")
 
-        # add bias to test vectors
-        testSet = NP.concatenate((testSet, NP.ones((numberOfTestSamples, 1))), axis=1)
-
         predictions = NP.zeros((numberOfTestSamples,), dtype=int)
 
         for cursor in range(numberOfTestSamples):
-                predictions[cursor] = Perceptron.__unitStep(NP.dot(self.__weight, testSet[cursor, :]))
+                predictions[cursor] = Perceptron.__unitStep(NP.dot(self.__weight, testSet[cursor, :]) + self.__bias)
 
         return predictions
 
@@ -122,32 +113,3 @@ class Perceptron():
         """
 
         return 1 if NP.sign(weightedSum) > 0 else 0
-
-    def test(self, testSet):
-        """
-        Trains the learning algorithm.
-        :param testSet: The test sets (numpy array).
-        :return: prediction list
-        """
-
-        if not isinstance(testSet, NP.ndarray):
-            testSet = NP.array(testSet)
-
-        try:
-            numberOfTestSamples = testSet.shape[0]
-            numOfTestFeatures = testSet.shape[1]
-
-        except Exception as e:
-            raise e
-
-        if self.__numOfFeatures != numOfTestFeatures:
-            raise ValueError("There is a mismatch between the number of training and test features.")
-
-        # init all weights and bias.from
-        testSet = NP.concatenate((testSet, NP.ones((numberOfTestSamples, 1))), axis=1)
-
-        predictionList = []
-        for cursor in range(numberOfTestSamples):
-                predictionList.append(Perceptron.__unitStep(NP.dot(self.__weight, testSet[cursor, :])))
-
-        return predictionList
